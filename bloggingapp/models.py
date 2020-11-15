73994a8cc8788630753084f4958936e03475cac6
Shubham Singhal <singhal121997@gmail.com>
2020-11-16 00:11:23 +0530

    create BlogModel

--- bloggingapp/models.py
+++ bloggingapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogModel(models.Model):
+	title = models.CharField(max_length=1000)
+	content = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.title
