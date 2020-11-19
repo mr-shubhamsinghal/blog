36f902e39b1fec246f71e2acd4152ba6c31c890c
Shubham Singhal <singhal121997@gmail.com>
2020-11-19 17:34:08 +0530

    use viewset

--- bloggingapp/urls.py
+++ bloggingapp/urls.py
@@ -1,13 +1,34 @@
 from django.urls import path
 
-from bloggingapp.views import blog_view, BlogAPIView, BlogListCreateAPIView, BlogRetrieveUpdateDestroyAPIView
+from bloggingapp.views import (blog_view, BlogAPIView, BlogViewSets,
+							   BlogListCreateAPIView,
+							   BlogRetrieveUpdateDestroyAPIView)
 
 
 app_name = 'blog_app'
 
+
+blog_list = BlogViewSets.as_view({
+		'get': 'list',
+	})
+
+blog_detail = BlogViewSets.as_view({
+		'get': 'retrieve',
+	})
+
+
 urlpatterns = [
     	path('fbv/', blog_view, name='blog_view'),
+
     	path('cbv_apiview/', BlogAPIView.as_view(), name='blog_api_view'),
-    	path('cbv_generic_lc_view/', BlogListCreateAPIView.as_view(), name='blog_generic_lc_view'),
-    	path('cbv_generic_rud_view/<int:pk>/', BlogRetrieveUpdateDestroyAPIView.as_view(), name='blog_generic_rud_view'),
+
+    	path('cbv_generic_lc_view/', BlogListCreateAPIView.as_view(),
+    		 name='blog_generic_lc_view'),
+
+    	path('cbv_generic_rud_view/<int:pk>/', BlogRetrieveUpdateDestroyAPIView.as_view(),
+    		 name='blog_generic_rud_view'),
+
+    	path('cbv_viewsets_list_view/', blog_list, name='blog_viewsets_list'),
+
+    	path('cbv_viewsets_detail_view/<int:pk>/', blog_detail, name='blog_viewsets_detail'),
     ]
